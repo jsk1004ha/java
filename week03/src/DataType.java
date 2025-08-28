ef6e1924b3cbd850274290e52c6a1a07a94657f6
jsk1004ha <129354802+jsk1004ha@users.noreply.github.com>
2025-08-28 20:36:06 +0900

    v0.5 DataType Conv

---
+++ week03/src/DataType.java
@@ -0,0 +1,14 @@
+public class DataType {
+    public static void main(String[] args) {
+        int score = 100;
+        float gpa = 4.13f;
+        System.out.println(score);
+        score = (int) 99.9;
+        System.out.println(score);
+        System.out.println(gpa);
+        score = (int) gpa;
+        System.out.println(score);
+        gpa = (float) score;
+        System.out.println(gpa);
+    }
+}
