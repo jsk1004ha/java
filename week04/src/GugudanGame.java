a92c9dcbb4b79fbe95140e8044ef0b143e805bc7
jsk1004ha <129354802+jsk1004ha@users.noreply.github.com>
2025-09-04 21:10:39 +0900

    v1.5 For & If

--- week04/src/GugudanGame.java
+++ week04/src/GugudanGame.java
@@ -6,8 +6,13 @@ public class GugudanGame {
         System.out.print("단을 입력하세요.: ");
         int dan = scanner.nextInt();
 
-        for(int i=1; i<10; i++){
-            System.out.println(dan + " x " + i + " = " + dan*i);
+        if(dan < 2| dan > 9){
+            System.out.println("단은 2~9의 값입니다.");
+        }else{
+            for(int i=1; i<10; i++){
+                System.out.println(dan + " x " + i + " = " + dan*i);
+        }
+
         }
     }
 }
