42f57415eb3f57d1ffa6c7c7e0493321a625e1bc
jsk1004ha <129354802+jsk1004ha@users.noreply.github.com>
2025-10-30 20:23:34 +0900

    exception handing v0.2

--- week08/src/ExceptionTest.java
+++ week08/src/ExceptionTest.java
@@ -4,10 +4,16 @@ public class ExceptionTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("input numerator: ");
+//        double numerator = scanner.nextInt();
         int numerator = scanner.nextInt();
         System.out.print("input demoninator: ");
+//        double demoninator = scanner.nextInt();
         int demoninator = scanner.nextInt();
 
-        System.out.println(numerator/demoninator);
+        if(demoninator == 0){
+            System.out.println("분모에는 0이 들어갈 수 없습니다.");
+        }else {
+            System.out.println(numerator / demoninator);
+        }
     }
 }
